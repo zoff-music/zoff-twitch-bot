bcc984794389c09edf8c3c941bce2f9b58ed5dc2
KasperRT <kasper@kasperrt.no>
2019-09-17 23:32:06 +0200

    Added exe

--- assets/program/setup.py
+++ assets/program/setup.py
@@ -1,12 +1,4 @@
 # setup.py
 from distutils.core import setup
 import py2exe
-setup(
-    console=['program.py'],
-    options={
-        'py2exe': {
-            'packages': ['socketio','tkinter','_thread','base64','json'],
-            'dist_dir': 'dist',
-            'compressed': True,
-        }
-    })
+setup(console=['program.py'], options={'py2exe': { 'packages': ['requests', 'socketio','tkinter','_thread','base64','json'], 'dist_dir': 'dist', 'compressed': True}})
